6799163792d1829f23befb2d0107760a807cfbcd
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-25 05:51:40 +0200

    Drawer item for for store data [work-in-progress]

---
+++ src/app/pg-drawer-item.ts
@@ -0,0 +1,9 @@
+import { LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("pg-drawer-item")
+class PGDrawerItem extends LitElement {
+    // TODO: delete list from "store", Add page, with "data: any[]", to "stack layout"
+}
+
+export default PGDrawerItem;
