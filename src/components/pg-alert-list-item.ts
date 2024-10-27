a7380ca7e399dd8c7a538f7451073f7258a89d40
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-27 02:37:05 +0100

    Add pg alert list item html

--- src/components/pg-alert-list-item.ts
+++ src/components/pg-alert-list-item.ts
@@ -1,4 +1,4 @@
-import { LitElement } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { Alert } from "../types";
 
@@ -7,7 +7,29 @@ class PGAlertListItem extends LitElement {
     @property({ type: Object, attribute: "data", reflect: false })
     data?: Alert;
 
-    // TODO: ...
+    // TODO: Continue here...
+    static get styles() {
+        return css`
+            div.number {
+                color: hsl(var(--ui-hsl-primary));
+                font-weight: bold;
+            }
+        `;
+    }
+
+    protected render() {
+        if (this.data === undefined) return html``;
+
+        return html`
+            <div class="title">${this.data.alert}</div>
+
+            <div class="number">
+                ${this.data.from === this.data.to
+                    ? this.data.from
+                    : `${this.data.from}..${this.data.to}`}
+            </div>
+        `;
+    }
 }
 
 export default PGAlertListItem;
