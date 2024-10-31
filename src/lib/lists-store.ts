786b5230bd9887f8f550277ca42e30088f4ffd94
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-31 07:36:54 +0100

    Add metal sheets validation method

--- src/lib/lists-store.ts
+++ src/lib/lists-store.ts
@@ -166,7 +166,41 @@ export class MetalSheetsStore extends ListsStore<"metalSheets"> {
     }
 
     validate(list: any): MetalSheet | null {
-        // TODO: Continue here...
+        if (typeof list.format !== "string") return null;
+
+        if (!list.toolID) list.toolID = "";
+        if (typeof list.toolID !== "string") return null;
+
+        if (!("data" in list)) return null;
+
+        if (typeof list.data !== "object") return null;
+        if (list.data.press !== "number") return null;
+
+        if (!("table" in list.data)) {
+            list.data.table = { header: [], data: [] };
+        } else {
+            if (typeof list.data.table !== "object") return null;
+
+            if (!("header" in list.data.table)) return null;
+            if (!("data" in list.data.table)) return null;
+
+            if (
+                !Array.isArray(list.data.table.header) ||
+                !Array.isArray(list.data.table.table)
+            ) {
+                return null;
+            }
+
+            for (const s of list.data.table.header) {
+                if (typeof s !== "string") return null;
+            }
+
+            for (const part of list.data.table.data) {
+                for (const s of part) {
+                    if (typeof s !== "string") return null;
+                }
+            }
+        }
 
         return list;
     }
