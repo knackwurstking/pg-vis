7a22bbe425fe09733aad8eeba49368cfe475fabf
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-01 23:27:19 +0100

    Add entry dialog & todo

---
+++ src/app/pages/metalSheets/pg-metal-sheet-entry-dialog.ts
@@ -0,0 +1,62 @@
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("pg-metal-sheet-entry-dialog")
+class PGMetalSheetEntryDialog extends LitElement {
+    @property({ type: Object, attribute: "header", reflect: true })
+    header?: string[];
+
+    /**
+     * Optional entry data, should have the same length liken the
+     * header property.
+     */
+    @property({ type: Object, attribute: "entry-data", reflect: true })
+    entryData: string[] = [];
+
+    protected createRenderRoot(): HTMLElement | DocumentFragment {
+        return this;
+    }
+
+    protected render() {
+        if (this.header !== undefined) {
+            // Prepare entry data,
+            if (this.header.length > this.entryData.length)
+                this.entryData = this.header.map(
+                    (_, index) => this.entryData[index] || "",
+                );
+        }
+
+        return html`
+            <ui-dialog name="entry" title="Eintrag Bearbeiten" modal inert>
+                <ui-flex-grid gap="0.25rem">
+                    ${[
+                        ...(this.header || []).map(
+                            (head, index) => html`
+                                <ui-flex-grid-item>
+                                    <ui-input
+                                        title="${head}"
+                                        type="text"
+                                        value="${this.entryData[index] || ""}"
+                                        @change=${() => {
+                                            // TODO: Update entry data
+                                        }}
+                                    ></ui-input>
+                                </ui-flex-grid-item>
+                            `,
+                        ),
+                    ]}
+                </ui-flex-grid>
+
+                <ui-button slot="actions" variant="full" color="secondary">
+                    Cancel
+                </ui-button>
+
+                <ui-button slot="actions" variant="full" color="primary">
+                    Submit
+                </ui-button>
+            </ui-dialog>
+        `;
+    }
+}
+
+export default PGMetalSheetEntryDialog;
