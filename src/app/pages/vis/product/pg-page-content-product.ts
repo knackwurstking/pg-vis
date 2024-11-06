95438b43938de1fc8189f6295e204604b0ef06ff
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-06 16:46:31 +0100

    Render data todos

--- src/app/pages/vis/product/pg-page-content-product.ts
+++ src/app/pages/vis/product/pg-page-content-product.ts
@@ -1,7 +1,8 @@
-import { html } from "lit";
+import { html, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
 import { styles } from "ui";
 import { Product } from "../../../../store-types";
+import PGApp from "../../../pg-app";
 import PGPageContent from "../../pg-page-content";
 
 @customElement("pg-page-contents-product")
@@ -34,11 +35,28 @@ class PGPageContentProduct extends PGPageContent<Product> {
                     </ui-flex-grid-item>
 
                     <ui-flex-grid-item>
-                        <!-- TODO: Vis data here -->
+                        ${this.renderData()}
                     </ui-flex-grid-item>
                 </ui-flex-grid>
             </div>
         `;
+    }
+
+    private renderData() {
+        if (this.data === undefined) return html``;
+
+        const visData = PGApp.queryStore().getData("visData");
+        if (visData === undefined) return html``;
+
+        const content: TemplateResult<1>[] = [];
+        for (const list of visData) {
+            // TODO: Add (`list.title`) title element to content
+            for (const _entry of list.data) {
+                // TODO: Check filter and create element, add to content
+            }
+        }
+
+        return html`${content}`;
     }
 }
 
