72889a5f9df669b9ea7cbf44ce9bf9d2804294c6
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-06 18:05:24 +0100

    Kick pg-add-product-dialog for now

--- src/app/dialogs/index.ts
+++ src/app/dialogs/index.ts
@@ -1,4 +1,3 @@
-//export { default as PGAddProductDialog } from "./pg-add-product-dialog";
 export { default as PGImportDialog } from "./pg-import-dialog";
 export { default as PGMetalSheetEntryDialog } from "./pg-metal-sheet-entry-dialog";
 export { default as PGMetalSheetTableDialog } from "./pg-metal-sheet-table-dialog";
