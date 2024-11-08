2b9477263dc4d3629e3f0220a0d58af07ec70b74
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-08 10:28:36 +0100

    Vis bookmarks page todos

--- src/app/pages/vis-bookmarks/pg-page-content-vis-bookmarks.ts
+++ src/app/pages/vis-bookmarks/pg-page-content-vis-bookmarks.ts
@@ -1,9 +1,9 @@
 import { customElement } from "lit/decorators.js";
-import { PGPageContent } from "..";
-import { Bookmarks } from "../../../store-types";
 import { html } from "ui";
+import { PGPageContent } from "..";
 import { PGApp } from "../..";
 import { newListsStore } from "../../../lib/lists-store";
+import { Bookmarks } from "../../../store-types";
 
 @customElement("pg-page-content-vis-bookmarks")
 class PGPageContentVisBookmarks extends PGPageContent<Bookmarks> {
@@ -13,7 +13,18 @@ class PGPageContentVisBookmarks extends PGPageContent<Bookmarks> {
                 ? newListsStore("visBookmarks").listKey(this.data)
                 : newListsStore("visBookmarks").title();
 
-        return html``; // TODO: Continue here...
+        return html`
+            <div
+                class="container no-scrollbar"
+                style="width: 100%; height: 100%; overflow: auto;"
+            >
+                ${this.renderData()}
+            </div>
+        `;
+    }
+
+    private renderData() {
+        return html``; // TODO: Iter data, but check "vis" store first
     }
 }
 
