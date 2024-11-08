1c4b8bfe2d1460f996193505160720ee041c03e1
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-08 17:10:58 +0100

    Render data, handle click

--- src/app/pages/vis-bookmarks/pg-page-content-vis-bookmarks.ts
+++ src/app/pages/vis-bookmarks/pg-page-content-vis-bookmarks.ts
@@ -1,9 +1,15 @@
 import { customElement } from "lit/decorators.js";
+import { keyed } from "lit/directives/keyed.js";
 import { html } from "ui";
-import { PGPageContent } from "..";
+
+import { PGPageContent, PGVisListItem } from "..";
+
 import { PGApp } from "../..";
+
 import { newListsStore } from "../../../lib/lists-store";
-import { Bookmarks } from "../../../store-types";
+import { queryTargetFromElementPath } from "../../../lib/query-utils";
+
+import { Bookmarks, PGStore, Product, Vis } from "../../../store-types";
 
 @customElement("pg-page-content-vis-bookmarks")
 class PGPageContentVisBookmarks extends PGPageContent<Bookmarks> {
@@ -13,18 +19,70 @@ class PGPageContentVisBookmarks extends PGPageContent<Bookmarks> {
                 ? newListsStore("visBookmarks").listKey(this.data)
                 : newListsStore("visBookmarks").title();
 
+        const listClickHandler = (ev: Event & { currentTarget: HTMLElement }) => {
+            if (!(ev.target instanceof Element)) return;
+
+            const target = queryTargetFromElementPath<PGVisListItem>(ev.target, "pg-vis-list-item");
+            if (target === null) return;
+
+            PGApp.queryStackLayout()!.setPage(
+                "product",
+                (page) => {
+                    const content = page.children[0] as PGPageContent<Product> | undefined;
+                    if (content !== undefined) {
+                        content.data = target.data;
+                    }
+                },
+                true,
+            );
+        }
+
         return html`
             <div
                 class="container no-scrollbar"
                 style="width: 100%; height: 100%; overflow: auto;"
             >
-                ${this.renderData()}
+                <div class="list" @click=${listClickHandler}>
+                    ${this.renderData()}
+                </div>
             </div>
         `;
     }
 
     private renderData() {
-        return html``; // TODO: Iter data, but check "vis" store first
+        if (this.data === undefined) return html``
+
+        const store = PGApp.queryStore()
+
+        const content = []
+        for (let product of this.data.data) {
+            product = this.productFromStore(store, product)
+            content.push(keyed(product, html`
+                <pg-vis-list-item data="${JSON.stringify(product)}"></pg-vis-list-item>
+            `))
+        }
+
+        return html`${content}`;
+    }
+
+    private productFromStore(store: PGStore, product: Product): Product {
+        const productKey = `${product.lotto} ${product.name}`
+        for (const list of this.sortVisLists(store.getData("vis") || [])) {
+            console.debug(`Search list "${list.title}" for the product key "${productKey}"`)
+            for (const listProduct of list.data) {
+                if (`${listProduct.lotto} ${listProduct.name}` === productKey) {
+                    console.debug(`Found "${productKey}" in "${list.title}"`)
+                    return product
+                }
+            }
+        }
+
+        console.warn(`Product key "${productKey}" not found`)
+        return product
+    }
+
+    private sortVisLists(lists: Vis[]): Vis[] {
+        return lists.sort((a, b) => a.date - b.date).reverse()
     }
 }
 
