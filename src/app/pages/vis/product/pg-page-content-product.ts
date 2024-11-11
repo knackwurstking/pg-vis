20f3ca6158d995843955b27846212b86b3d85994
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-11 04:06:32 +0100

    Allow edit vis data entry from the product page

--- src/app/pages/vis/product/pg-page-content-product.ts
+++ src/app/pages/vis/product/pg-page-content-product.ts
@@ -1,12 +1,11 @@
-import { html, PropertyValues } from "lit";
+import { html, nothing, PropertyValues } from "lit";
 import { customElement } from "lit/decorators.js";
-
-import { nothing } from "lit";
 import { UIFlexGridItem } from "ui";
+import * as lib from "../../../../lib";
 import { Product } from "../../../../store-types";
 import PGApp from "../../../pg-app";
 import PGPageContent from "../../pg-page-content";
-import { PGVisDataListItem } from "../../vis-data";
+import { PGPageContentVisDataEdit, PGVisDataListItem } from "../../vis-data";
 
 @customElement("pg-page-contents-product")
 class PGPageContentProduct extends PGPageContent<Product> {
@@ -17,6 +16,30 @@ class PGPageContentProduct extends PGPageContent<Product> {
      */
 
     protected render() {
+        const handleListClick = (ev: Event) => {
+            if (!(ev.target instanceof Element) || this.data === undefined) return;
+
+            const target = lib.queryTargetFromElementPath<PGVisDataListItem>(
+                ev.target,
+                "pg-vis-data-list-item",
+            );
+            if (target === null) return;
+
+            PGApp.queryStackLayout()!.setPage(
+                "visDataEdit",
+                (page) => {
+                    const content = page.children[0] as PGPageContentVisDataEdit | undefined;
+
+                    if (content !== undefined) {
+                        content.data = target.data;
+                        content.listKey = target.getAttribute("data-listKey") || undefined;
+                        content.entryIndex = target.entryIndex;
+                    }
+                },
+                true,
+            );
+        };
+
         return html`
             <div class="container no-scrollbar" style="width: 100%; height: 100%; overflow: auto;">
                 <ui-flex-grid gap="0.25rem">
@@ -40,7 +63,12 @@ class PGPageContentProduct extends PGPageContent<Product> {
                         </ui-flex-grid-item>
                     </ui-flex-grid-row>
 
-                    <ui-flex-grid class="list" direction="column" gap="0.25rem">
+                    <ui-flex-grid
+                        class="list"
+                        direction="column"
+                        gap="0.25rem"
+                        @click=${handleListClick}
+                    >
                         ${nothing}
                     </ui-flex-grid>
                 </ui-flex-grid>
@@ -61,11 +89,13 @@ class PGPageContentProduct extends PGPageContent<Product> {
         if (visData === undefined) return html``;
 
         const container = this.querySelector(`.list`)!;
+        const listsStore = lib.listsStore("visData");
 
         let entryIndex = -1;
         for (const list of visData) {
             entryIndex++;
 
+            const listKey = listsStore.listKey(list);
             let hasHeading = false;
             for (const entry of list.data) {
                 setTimeout(() => {
@@ -95,8 +125,11 @@ class PGPageContentProduct extends PGPageContent<Product> {
                     }
 
                     const item = new PGVisDataListItem();
+                    item.style.cursor = "pointer";
+                    item.role = "button";
                     item.data = entry;
                     item.entryIndex = entryIndex;
+                    item.setAttribute("data-listKey", listKey);
                     container.appendChild(item);
                 });
             }
