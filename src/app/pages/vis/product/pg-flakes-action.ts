ae7db624f4bb8560d69f2366360d4e2dbaf70b5c
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-11 04:15:13 +0100

    Remove todo

--- src/app/pages/vis/product/pg-flakes-action.ts
+++ src/app/pages/vis/product/pg-flakes-action.ts
@@ -2,8 +2,6 @@ import { LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 
 @customElement("pg-flakes-action")
-class PGFlakesAction extends LitElement {
-    // TODO: Label checkbox for enabling flakes mode for this product item
-}
+class PGFlakesAction extends LitElement {}
 
 export default PGFlakesAction;
