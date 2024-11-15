1ca04948879a27aaf4fb16b20616d899ac55e9da
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-15 09:41:11 +0100

    Update todos

--- src/app/pages/main/special/pg-page-content-special.ts
+++ src/app/pages/main/special/pg-page-content-special.ts
@@ -2,10 +2,26 @@ import { customElement } from "lit/decorators.js";
 
 import * as app from "@app";
 import * as types from "@types";
+import { html } from "lit";
 
 @customElement("pg-page-content-special")
 class PGPageContentSpecial extends app.PGPageContent<types.Special> {
-    // TODO: Continue here...
+    protected render() {
+        switch (this.data?.type) {
+            case "flakes":
+                this.renderFlakes();
+                break;
+
+            default:
+                return html``;
+        }
+
+        return html``;
+    }
+
+    private renderFlakes() {
+        return html``; // TODO: Continue here...
+    }
 }
 
 export default PGPageContentSpecial;
