2b411e01361cc13f0711efdfa77d4f131b7c7dc4
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-15 19:57:28 +0100

    The basic table to render & more todos

--- src/app/pages/main/special/pg-page-content-special.ts
+++ src/app/pages/main/special/pg-page-content-special.ts
@@ -3,6 +3,7 @@ import { customElement } from "lit/decorators.js";
 
 import * as app from "@app";
 import * as types from "@types";
+import { repeat } from "lit/directives/repeat.js";
 
 // TODO: Convert table to pdf for type "flakes"
 @customElement("pg-page-content-special")
@@ -23,7 +24,35 @@ class PGPageContentSpecial extends app.PGPageContent<types.Special> {
     }
 
     private renderFlakes(data: types.FlakesData[]) {
-        return html``;
+        return html`
+            <div class="no-scrollbar" style="width: 100%; overflow-x: auto">
+                ${repeat(
+                    data,
+                    (data) => `${data.press}`,
+                    (data, index) => html`
+                        <table>
+                            <thead>
+                                <tr>
+                                    <!-- TODO: Table Heading 1: Press info (colspan: 100%) -->
+                                </tr>
+                                <tr>
+                                    <!-- TODO: Table Heading 2: compatatore & (secondary) slots -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- TODO: Render compatatore speed -->
+                                ${repeat(
+                                    data.secondary,
+                                    (secondaryData) =>
+                                        `${secondaryData.slot},${secondaryData.percent},${secondaryData.value}`,
+                                    (secondaryData, index) => html``, // TODO: ...
+                                )}
+                            </tbody>
+                        </table>
+                    `,
+                )}
+            </div>
+        `;
     }
 }
 
