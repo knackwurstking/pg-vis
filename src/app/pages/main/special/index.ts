9a2cc7251552385403a627149954204a51565432
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-15 09:38:10 +0100

    @todo: special page, type flakes

---
+++ src/app/pages/main/special/index.ts
@@ -0,0 +1,1 @@
+export { default as PGPageContentSpecial } from "./pg-page-content-special";
---
+++ src/app/pages/main/special/pg-page-content-special.ts
@@ -0,0 +1,11 @@
+import { customElement } from "lit/decorators.js";
+
+import * as app from "@app";
+import * as types from "@types";
+
+@customElement("pg-page-content-special")
+class PGPageContentSpecial extends app.PGPageContent<types.Special> {
+    // TODO: Continue here...
+}
+
+export default PGPageContentSpecial;
