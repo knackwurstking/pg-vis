b86f5953c905960f97a357ef485abe00ed78547b
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-17 11:22:39 +0100

    Add isFlakesProduct js util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,11 @@ export function productKey(product: types.Product): string {
     return `${product.lotto} ${product.name}`;
 }
 
+export function isFlakesProduct(store: types.PGStore, product: types.Product): boolean {
+    const flakes = store.getData("special")?.find((data) => data.type === "flakes");
+    return flakes?.products.find((lotto) => lotto === product.lotto) !== undefined;
+}
+
 export function listStore<T extends keyof lib.listStores.ListStoreData>(
     key: T,
 ): lib.listStores.ListStore<T> {
