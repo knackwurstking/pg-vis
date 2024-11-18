9b53f59ecbe0cd406b09ab1f8073081c438e1ce1
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-18 08:08:55 +0100

    v1.1.1 release

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const version = "v1.1.0.dev"; // TODO: Don't forget to remove the ".dev" suffix before release
-export const build = 9; // NOTE: v1.1.0 build
+export const version = "v1.1.1";
+export const build = 10; // NOTE: v1.1.1 build
