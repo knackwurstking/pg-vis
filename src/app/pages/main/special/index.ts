4581fa9fdab1c0b220a109f7cb670e55dcfe38ab
Udo Bauer <knackwurstking.tux@gmail.com>
2024-11-18 09:55:17 +0100

    Remove unused component filter bar

--- src/app/pages/main/special/index.ts
+++ src/app/pages/main/special/index.ts
@@ -1,3 +1,2 @@
-export { default as PGFlakesFilterBar } from "./pg-flakes-filter-bar";
 export { default as PGPageContentSpecial } from "./pg-page-content-special";
 export { default as PGPageContentFlakes } from "./pg-page-content-flakes";
