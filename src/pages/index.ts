663e5f7350174360329aff43c5b89aa7f0a78bd8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-20 19:41:34 +0100

    Add pages directory

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export * as alertLists from "./alert-lists";
+export * as metalSheets from "./metal-sheets";
+export * as special from "./special";
+export * as vis from "./vis";
+export * as visBookmarks from "./vis-bookmarks";
+export * as visData from "./vis-data";
