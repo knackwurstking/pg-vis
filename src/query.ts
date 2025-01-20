b2de18a2e5d071d582b22ca1fc8120bdb3421ce8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-20 15:58:04 +0100

    Add all missing AppBar icons

--- src/query.ts
+++ src/query.ts
@@ -1,3 +1,5 @@
+// App Bar Elements
+
 export function appBar_ButtonOpenDrawer(): HTMLButtonElement {
     return document.querySelector(`#appBarButtonOpenDrawer`)!;
 }
@@ -8,4 +10,24 @@ export function appBar_ButtonBack(): HTMLButtonElement {
 
 export function appBar_Title(): HTMLHeadingElement {
     return document.querySelector(`#appBarTitle`)!;
+}
+
+export function appBar_ButtonEdit(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonEdit`)!;
+}
+
+export function appBar_ButtonShare(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonShare`)!;
+}
+
+export function appBar_ButtonSearch(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonSearch`)!;
+}
+
+export function appBar_ButtonTrash(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonTrash`)!;
+}
+
+export function appBar_ButtonPrinter(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonPrinter`)!;
 }
