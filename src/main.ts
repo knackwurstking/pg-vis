9e5c4aa097e7e8b342a8fddedb4037799e72a17f
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-20 02:34:20 +0100

    Add left and center app bar items

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { registerSW } from "virtual:pwa-register";
 
+import * as query from "./query";
+
 const updateSW = registerSW({
     async onNeedRefresh() {
         if (confirm(`Update available`)) {
@@ -7,5 +9,12 @@ const updateSW = registerSW({
         }
     },
 });
+
+// Initialize AppBar button handlers
+
+const openDrawerButton = query.appBar_ButtonOpenDrawer();
+openDrawerButton.onclick = () => {
+    // TODO: Open the drawer
+};
 
 // TODO: Router setup here
---
+++ src/query.ts
@@ -0,0 +1,11 @@
+export function appBar_ButtonOpenDrawer(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonOpenDrawer`)!;
+}
+
+export function appBar_ButtonBack(): HTMLButtonElement {
+    return document.querySelector(`#appBarButtonBack`)!;
+}
+
+export function appBar_Title(): HTMLHeadingElement {
+    return document.querySelector(`#appBarTitle`)!;
+}
