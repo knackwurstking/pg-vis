b883e1c1a30cbbf8988d6e273d1498f318091904
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-20 17:09:51 +0100

    Kick version and build from globals

--- src/globals.ts
+++ src/globals.ts
@@ -2,8 +2,6 @@ import * as ui from "ui";
 
 import * as types from "./types";
 
-export const version = "v2.0.0";
-export const build = 15; // NOTE: v2.0.0 build
 export const store = createStore();
 
 function createStore(): types.PGStore {
