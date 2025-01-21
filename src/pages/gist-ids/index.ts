3ac73de659b1d0b92ed772e1cc0609800e4306bd
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-21 06:27:02 +0100

    Iter gist-id input elements

--- src/pages/gist-ids/index.ts
+++ src/pages/gist-ids/index.ts
@@ -1,5 +1,4 @@
 import * as query from "../../utils-query";
-import * as init from "./init";
 
 let originTitle: string = "";
 
@@ -8,11 +7,47 @@ export async function onMount() {
     originTitle = appBarTitle.innerText;
     appBarTitle.innerText = "Gist IDs";
 
-    init.appBar();
-    init.gistIDInputs();
-    init.autoUpdatesCheckboxes();
+    gistIDInputs();
+    autoUpdatesCheckboxes();
 }
 
 export async function onDestroy() {
     query.appBar_Title().innerText = originTitle;
+}
+
+function gistIDInputs() {
+    const inputElements = document.querySelectorAll<HTMLInputElement>(
+        `.gist-ids .gist-id-container input[type="text"]`,
+    );
+
+    for (const el of inputElements) {
+        switch (el.id) {
+            case "gistID_AlertLists":
+                // TODO: ...
+                break;
+
+            case "gistID_MetalSheets":
+                // TODO: ...
+                break;
+
+            case "gistID_VIS":
+                // TODO: ...
+                break;
+
+            case "gistID_VISData":
+                // TODO: ...
+                break;
+
+            case "gistID_Special":
+                // TODO: ...
+                break;
+
+            default:
+                throw new Error(`unknown id "${el.id}"`);
+        }
+    }
+}
+
+function autoUpdatesCheckboxes() {
+    // TODO: ...
 }
