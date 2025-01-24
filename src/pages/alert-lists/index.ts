c828f61fd4d057fe4b20501a122a34492db08a6e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-24 10:39:14 +0100

    Get data for listKey

--- src/pages/alert-lists/index.ts
+++ src/pages/alert-lists/index.ts
@@ -1,15 +1,25 @@
 import * as query from "../../utils-query";
+import * as globals from "../../globals";
+import * as listsStore from "../../list-stores";
 
 let originTitle: string = "";
+const ls = listsStore.get("alert-lists");
 
 export async function onMount() {
     const param = query.getSearchParam();
 
+    const list = globals.store.get("alert-lists")!.lists.find((list) => {
+        return ls.listKey(list) === param.listKey;
+    });
+    if (!list) {
+        throw new Error(`Unknown list key: ${param.listKey}`);
+    }
+
     const appBarTitle = query.appBar_Title();
     originTitle = appBarTitle.innerText;
-    appBarTitle.innerText = decodeURIComponent(param.listKey);
+    appBarTitle.innerText = decodeURIComponent(list.title);
 
-    // TODO: ...
+    console.debug(list); // TODO: Render items and init app bar action buttons
 }
 
 export async function onDestroy() {
