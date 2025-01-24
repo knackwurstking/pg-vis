6245cbdaa437b6c2b2ec11722726590ad64aee38
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-24 23:54:35 +0100

    Add alert list content

--- src/pages/alert-lists/create/alert-item.ts
+++ src/pages/alert-lists/create/alert-item.ts
@@ -1,14 +1,43 @@
+import * as ui from "ui";
 import * as types from "../../../types";
 
 const html = String.raw;
 
-export function alertItem(alert: types.Alert): types.Component<HTMLLIElement> {
+export interface AlertItemProps {
+    enableRouting?: boolean;
+}
+
+export function alertItem(
+    alert: types.Alert,
+    props?: AlertItemProps | null,
+): types.Component<HTMLLIElement> {
     const el = document.createElement("li");
 
-    el.className = "alert-item";
-    el.style.width = "100%";
+    el.className = "alert-item ui-flex nowrap align-center justify-between";
 
-    el.innerHTML = html` <!-- TODO: ... --> `;
+    el.style.width = "100%";
+    el.style.padding = "var(--ui-spacing)";
+    el.style.borderBottom = "1px solid hsl(var(--ui-border-color)";
+
+    if (!!props?.enableRouting) {
+        el.role = "button";
+        el.style.cursor = "pointer";
+
+        // TODO: Go to alert detail page "?listKey=...&index=...#alert"
+    }
+
+    el.innerHTML = html`
+        <p>${alert.alert}</p>
+        <p
+            style="${ui.styles({
+                color: "var(--ui-primary)",
+                textWrap: "nowrap",
+                marginLeft: "var(--ui-spacing)",
+            } as CSSStyleDeclaration)}"
+        >
+            ${alert.from === alert.to ? alert.from : `${alert.from}..${alert.to}`}
+        </p>
+    `;
 
     return {
         element: el,
