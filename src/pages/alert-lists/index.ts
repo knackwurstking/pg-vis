eb6e82959009c49781971a7e6a9b608083fe1e46
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-25 19:32:29 +0100

    Add a search bar to alert lists page

--- src/pages/alert-lists/index.ts
+++ src/pages/alert-lists/index.ts
@@ -2,6 +2,7 @@ import * as globals from "../../globals";
 import * as types from "../../types";
 import * as query from "../../utils-query";
 import * as create from "./create";
+import * as utils from "../../utils";
 
 let cleanup: (() => void)[] = [];
 let originTitle: string = "";
@@ -28,7 +29,10 @@ export async function onDestroy() {
 }
 
 function render(alerts: types.Alert[], listKey: string) {
-    const container = query.routerTarget().querySelector<HTMLUListElement>(`.alerts`)!;
+    const searchBarInput = query
+        .routerTarget()
+        .querySelector<HTMLInputElement>(`.search-bar input[type="search"]`)!;
+    const alertsContainer = query.routerTarget().querySelector<HTMLUListElement>(`.alerts`)!;
 
     for (let x = 0; x < alerts.length; x++) {
         const item = create.alertItem({
@@ -39,6 +43,22 @@ function render(alerts: types.Alert[], listKey: string) {
             },
         });
         cleanup.push(item.destroy);
-        container.appendChild(item.element);
+        alertsContainer.appendChild(item.element);
     }
+
+    searchBarInput.oninput = () => {
+        const r = utils.generateRegExp(searchBarInput.value);
+        for (const item of [...alertsContainer.children]) {
+            if (item.textContent === null) {
+                continue;
+            }
+
+            if (!!item.textContent.match(r)) {
+                // Show
+                (item as HTMLElement).style.display = "flex";
+            } else {
+                (item as HTMLElement).style.display = "none";
+            }
+        }
+    };
 }
