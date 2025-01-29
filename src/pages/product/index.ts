6ab504db560223611cf0da50a8d1dff045f80ad3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-29 20:12:20 +0100

    Enable back button

--- src/pages/product/index.ts
+++ src/pages/product/index.ts
@@ -15,15 +15,26 @@ let originTitle: string = "";
 export async function onMount() {
     const param: Param = ui.router.hash.getSearchParam();
 
+    // Get product from vis
     const product = globals.getProduct(param.listKey || "", parseInt(param.index || "-1", 10));
     if (!product) {
         throw `product not found: listKey=${param.listKey}, index=${param.index}`;
     }
 
+    // Set app bar title
     {
         const appBarTitle = query.appBar_Title();
         originTitle = appBarTitle.innerText;
         appBarTitle.innerText = `${product.lotto}`;
+    }
+
+    // Enable back button
+    {
+        const backButton = query.appBar_ButtonBack();
+        backButton.style.display = "inline-flex";
+        cleanup.push(() => {
+            backButton.style.display = "none";
+        });
     }
 
     render(product);
