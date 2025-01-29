e13b9b22001596401de936f0c195af4410c6d891
Udo Bauer <knackwurstking.tux@gmail.com>
2025-01-29 06:10:27 +0100

    Add drawer vis-item

--- src/drawer/create/index.ts
+++ src/drawer/create/index.ts
@@ -1,2 +1,3 @@
 export * from "./alert-list-item";
 export * from "./metal-sheet-item";
+export * from "./vis-item";
---
+++ src/drawer/create/vis-item.ts
@@ -0,0 +1,52 @@
+import * as globals from "../../globals";
+import * as listsStore from "../../list-stores";
+import * as types from "../../types";
+
+const html = String.raw;
+const ls = listsStore.get("vis");
+
+export interface VISItemProps {
+    data: types.Vis;
+}
+
+export function visItem(props: VISItemProps): types.Component<HTMLLIElement> {
+    const el = document.createElement("li");
+
+    el.className = "ui-flex justify-between";
+
+    ls.title();
+    el.innerHTML = html`
+        <a
+            class="ui-flex column align-start justify-center"
+            style="width: 100%; height: 100%;"
+            href="#vis?listKey=${ls.listKey(props.data)}"
+        >
+            <span>${props.data.title}</span>
+            <span>${props.data.data.length} Einträge</span>
+        </a>
+
+        <button class="delete" variant="ghost" color="destructive">
+            <i class="bi bi-trash"></i>
+        </button>
+    `;
+
+    {
+        const deleteButton = el.querySelector<HTMLButtonElement>(`button.delete`)!;
+        deleteButton.onclick = () => {
+            globals.store.update("vis", (data) => {
+                const key = ls.listKey(props.data);
+
+                if (confirm(`"${key}" wirklich löschen?`)) {
+                    data.lists = data.lists.filter((list) => ls.listKey(list) !== key);
+                }
+
+                return data;
+            });
+        };
+    }
+
+    return {
+        element: el,
+        destroy() {},
+    };
+}
