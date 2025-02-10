3517c44ca95491cbcc310e77a904e3d8d1868e8c
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-10 10:14:35 +0100

    Clean up special-flakes-entry dialog & +FIXME

--- src/dialogs/special-flakes-entry.ts
+++ src/dialogs/special-flakes-entry.ts
@@ -6,53 +6,44 @@ const html = String.raw;
 function init(entry?: types.SpecialFlakesEntry | null): types.Component<
     HTMLDialogElement,
     {
-        close: HTMLButtonElement;
-        inputs: HTMLElement;
-        reset: HTMLInputElement;
+        inputContainer: HTMLElement;
     },
     { open: () => Promise<types.SpecialFlakesEntry | null> }
 > {
     const root = document.querySelector<HTMLDialogElement>(`dialog[name="special-flakes-entry"]`)!;
 
     const query = {
-        close: root.querySelector<HTMLButtonElement>(`button.close`)!,
-        inputs: root.querySelector<HTMLElement>(`.inputs`)!,
-        reset: root.querySelector<HTMLInputElement>(`input[type="reset"]`)!,
+        inputContainer: root.querySelector<HTMLElement>(`.input-container`)!,
     };
 
     const open: () => Promise<types.SpecialFlakesEntry | null> = () => {
         return new Promise((resolve, _reject) => {
-            let canceled = false;
-            query.close.onclick = () => {
-                canceled = true;
-                root.close();
+            let result: types.SpecialFlakesEntry | null = null;
+
+            root.onclose = () => {
+                resolve(result);
             };
 
             root.onclose = () => {
-                if (canceled) {
-                    resolve(null);
-                    return;
-                }
-
                 // Get the values from the dialog form inputs
-                const inputs = query.inputs.querySelectorAll(`input`);
-                const select = query.inputs.querySelector<HTMLSelectElement>("select")!;
+                const inputElements = query.inputContainer.querySelectorAll(`input`);
+                const select = query.inputContainer.querySelector<HTMLSelectElement>("select")!;
 
-                const result: types.SpecialFlakesEntry = {
+                result = {
                     press:
                         entry?.press ||
                         (select.options[select.selectedIndex]
                             .value as types.SpecialFlakes_PressSlot),
 
-                    compatatore: parseInt(inputs[1].value, 10),
+                    compatatore: parseInt(inputElements[1].value, 10),
 
                     primary: {
                         ...(entry?.primary || {}),
-                        value: parseInt(inputs[0].value || "0", 10),
+                        value: parseInt(inputElements[0].value || "0", 10),
                         percent: 0,
                     },
 
-                    secondary: [...query.inputs.querySelectorAll(`.tower-slot`)]
+                    secondary: [...query.inputContainer.querySelectorAll(`.tower-slot`)]
                         .map((el, index) => {
                             const [inputPercent, inputSpeed] = el.querySelectorAll("input");
                             const percent = parseInt(inputPercent.value, 10);
@@ -69,14 +60,14 @@ function init(entry?: types.SpecialFlakesEntry | null): types.Component<
                 result.primary.percent = eval(
                     "100 -" + result.secondary.map((e) => `${e.percent}`).join(" - "),
                 );
-
-                resolve(result);
             };
 
-            const initForm = () => {
-                query.inputs.innerHTML = html``;
+            query.inputContainer.innerHTML = html``;
 
-                const firstRow = document.createElement("div");
+            const firstRow = document.createElement("div");
+            query.inputContainer.appendChild(firstRow);
+
+            {
                 firstRow.className = "ui-flex-grid-row";
                 firstRow.style.setProperty("--align", "center");
                 firstRow.style.setProperty("--justify", "space-evenly");
@@ -115,60 +106,52 @@ function init(entry?: types.SpecialFlakesEntry | null): types.Component<
 
                 firstRow.querySelector<HTMLSelectElement>(`select`)!.selectedIndex =
                     globals.flakesPressSlots.indexOf(entry?.press || globals.flakesPressSlots[0]);
+            }
 
-                query.inputs.appendChild(firstRow);
+            // FIXME: The following elements are overflowing
+            globals.flakesTowerSlots.forEach((slot) => {
+                const { percent, value } = entry?.secondary.find((e) => e.slot === slot) || {
+                    percent: 0,
+                    value: 0,
+                };
 
-                globals.flakesTowerSlots.forEach((slot) => {
-                    const { percent, value } = entry?.secondary.find((e) => e.slot === slot) || {
-                        percent: 0,
-                        value: 0,
-                    };
+                const item = document.createElement("div");
+                query.inputContainer.appendChild(item);
 
-                    const item = document.createElement("div");
-                    item.className = "ui-flex-grid-item";
-                    item.innerHTML = html`
-                        <div class="tower-slot ui-flex-grid-item">
-                            <h5>${slot}</h5>
+                item.className = "ui-flex-grid-item";
+                item.innerHTML = html`
+                    <div class="tower-slot ui-flex-grid-item">
+                        <h5>${slot}</h5>
 
-                            <div
-                                class="ui-flex-grid-row"
-                                style="--align: center; --justify: space-between"
-                            >
-                                <div class="ui-flex-grid" style="--gap: 0">
-                                    <label>Prozent</label>
-                                    <input
-                                        class="input"
-                                        id="specialFlakesEntry_${slot}-percent"
-                                        style="text-align: center;"
-                                        type="number"
-                                        value="${percent || ""}"
-                                    />
-                                </div>
+                        <div
+                            class="ui-flex-grid-row"
+                            style="--align: center; --justify: space-between"
+                        >
+                            <div class="ui-flex-grid" style="--gap: 0">
+                                <label>Prozent</label>
+                                <input
+                                    class="input"
+                                    id="specialFlakesEntry_${slot}-percent"
+                                    style="text-align: center;"
+                                    type="number"
+                                    value="${percent || ""}"
+                                />
+                            </div>
 
-                                <div class="ui-flex-grid" style="--gap: 0">
-                                    <label>Geschwindigkeit</label>
-                                    <input
-                                        class="input"
-                                        id="specialFlakesEntry_${slot}-speed"
-                                        style="text-align: center;"
-                                        type="number"
-                                        value="${value || ""}"
-                                    />
-                                </div>
+                            <div class="ui-flex-grid" style="--gap: 0">
+                                <label>Geschwindigkeit</label>
+                                <input
+                                    class="input"
+                                    id="specialFlakesEntry_${slot}-speed"
+                                    style="text-align: center;"
+                                    type="number"
+                                    value="${value || ""}"
+                                />
                             </div>
                         </div>
-                    `;
-                    query.inputs.appendChild(item);
-                });
-            };
-
-            initForm();
-
-            query.reset.onclick = (e) => {
-                if (!entry) return;
-                e.preventDefault();
-                initForm();
-            };
+                    </div>
+                `;
+            });
 
             root.showModal();
         });
